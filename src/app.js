d664009e916c142b5a389019388f18cf5177e200
BasCart <b.cartagena.paredes@gmail.com>
2025-02-11 02:22:49 +0000

    create a generate of escuse

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let a= Math.floor(Math.random()*who.length);
+  let aa= who[a];
+  let b= Math.floor(Math.random()*action.length);
+  let bb= action[b];
+  let c= Math.floor(Math.random()*what.length);
+  let cc= what[c];
+  let d= Math.floor(Math.random()*when.length);
+  let dd= when[d];
+  let frase= aa+bb+cc+dd;
+
+  document.getElementById("excuse").innerHTML = frase;
+  return frase;
 };
