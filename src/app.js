350017318547b2a6bdda1af047d7f42484a5c081
BasCart <b.cartagena.paredes@gmail.com>
2025-02-15 19:11:54 +0000

    renaming variables and add templates literals

--- src/app.js
+++ src/app.js
@@ -6,20 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-
-  let a= Math.floor(Math.random()*who.length);
-  let aa= who[a];
-  let b= Math.floor(Math.random()*action.length);
-  let bb= action[b];
-  let c= Math.floor(Math.random()*what.length);
-  let cc= what[c];
-  let d= Math.floor(Math.random()*when.length);
-  let dd= when[d];
-  let frase= aa+bb+cc+dd;
+  
+  let randomElementWho= Math.floor(Math.random()*who.length);
+  let elementWho= who[randomElementWho];
+  
+  let randomElementAction= Math.floor(Math.random()*action.length);
+  let elementAction= action[randomElementAction];
+  
+  let randomElementWhat= Math.floor(Math.random()*what.length);
+  let elementWhat= what[randomElementWhat];
+  
+  let randomElementWhen= Math.floor(Math.random()*when.length);
+  let elementWhen= when[randomElementWhen];
+  
+  let frase= `${elementWho} ${elementAction} ${elementWhat} ${elementWhen}`;
 
   document.getElementById("excuse").innerHTML = frase;
   return frase;
