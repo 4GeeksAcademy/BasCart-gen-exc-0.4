7cdb4efb8721f3866a14be9f813c4e1ab5c563f5
BasCart <b.cartagena.paredes@gmail.com>
2025-02-20 03:30:12 +0000

    optimization and best practices

--- src/app.js
+++ src/app.js
@@ -5,27 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  
+window.onload = function () {
+
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
   
-  let randomElementWho= Math.floor(Math.random()*who.length);
-  let elementWho= who[randomElementWho];
-  
-  let randomElementAction= Math.floor(Math.random()*action.length);
-  let elementAction= action[randomElementAction];
-  
-  let randomElementWhat= Math.floor(Math.random()*what.length);
-  let elementWhat= what[randomElementWhat];
-  
-  let randomElementWhen= Math.floor(Math.random()*when.length);
-  let elementWhen= when[randomElementWhen];
-  
-  let frase= `${elementWho} ${elementAction} ${elementWhat} ${elementWhen}`;
+  function elementRandom(arr){
+
+    return arr[Math.floor(Math.random()* arr.length)];
+  }
+
+  let  frase= `${elementRandom(who)} ${elementRandom(action)} ${elementRandom(what)} ${elementRandom(when)}`
 
   document.getElementById("excuse").innerHTML = frase;
   return frase;
+  
 };
+
